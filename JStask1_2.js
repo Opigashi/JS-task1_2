5ff0344788cd7825b9ffa155a24fe8134d01a0cf
Ayaka Ohigashi <a.ohigashi.0129@gmail.com>
2023-10-08 20:31:33 +0900

    second commit

--- JStask1_2.js
+++ JStask1_2.js
@@ -1,1 +1,11 @@
-//test
+'use strict';
+
+while(true){
+    let command = prompt('日本の首都は？');
+    
+    if(command === '東京'){
+        alert('正解です');
+    } else {
+        alert('不正解です！');
+    }
+}
